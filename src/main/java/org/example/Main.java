c451707cb13669130a2f076282d1d02fa7b31203
Aryan-Labbalabadubdub <aryanhemmati1382@gmail.com>
2024-04-06 22:51:37 +0330

    added min radius for balls

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,8 @@ import javax.swing.*;
 import java.awt.geom.Point2D;
 import java.util.Random;
 
+import static controller.Constants.*;
+
 public class Main {
     static Random rng=new Random();
     public static void main(String[] args) {
@@ -17,10 +19,10 @@ public class Main {
             GlassFrame.getINSTANCE();
             MotionPanel.getINSTANCE();
             new Update();
-            for(int i = 0; i< Constants.NUMBER_OF_BALLS; i++){
+            for(int i = 0; i< NUMBER_OF_BALLS; i++){
                 double randomX=rng.nextDouble(MotionPanel.getINSTANCE().getX(),MotionPanel.getINSTANCE().getX()+MotionPanel.getINSTANCE().getWidth());
                 double randomY=rng.nextDouble(MotionPanel.getINSTANCE().getY(),MotionPanel.getINSTANCE().getY()+MotionPanel.getINSTANCE().getHeight());
-                double randomRadius=rng.nextDouble(0, Constants.MAX_RADIUS);
+                double randomRadius=rng.nextDouble(MIN_RADIUS, MAX_RADIUS);
                 new BallModel(new Point2D.Double(randomX,randomY),randomRadius);
             }
         });
